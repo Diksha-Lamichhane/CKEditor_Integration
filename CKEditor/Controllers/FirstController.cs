bec728abc81b000d39e2ac2e88dbc6e07559b060
Diksha-Lamichhane <lamichhanediksha17@gmail.com>
2022-04-19 16:54:30 +0545

    first commit

---
+++ CKEditor/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CKEditor.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
